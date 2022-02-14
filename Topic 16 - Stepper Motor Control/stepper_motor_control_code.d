ebfebd01b683cddb7416c6ae4d1bc205fa0abb14
Santosh <88974296+Santosh2611@users.noreply.github.com>
2022-02-15 04:59:58 +0530

    Add files via upload

---
+++ Topic 16 - Stepper Motor Control/stepper_motor_control_code.d
@@ -0,0 +1,3 @@
+stepper_motor_control_code.o: Stepper_Motor_Control_Code.c
+stepper_motor_control_code.o: C:\Keil\ARM\INC\Philips\lpc214x.h
+stepper_motor_control_code.o: C:\Keil\ARM\RV31\INC\stdint.h
