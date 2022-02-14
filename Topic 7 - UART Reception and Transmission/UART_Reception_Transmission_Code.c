5a4d617890deaa5d8a1d46adf6b68bd78f5873d2
Santosh <88974296+Santosh2611@users.noreply.github.com>
2022-02-15 04:33:14 +0530

    Create UART_Reception_Transmission_Code.c

---
+++ Topic 7 - UART Reception and Transmission/UART_Reception_Transmission_Code.c
@@ -0,0 +1,22 @@
+#include<lpc214x.h> // To define all Special Function Registers (SFRs) - Address Mapping     
+
+unsigned char data;
+
+// Main Function:
+int main (void)
+{
+	// Execution Starts Here:
+	PINSEL0 = 0x00000005; // Set P0.0 Pin as TxD and P0.1 Pin as RxD (UART0)
+	U0LCR = 0x83; // Set 8 Bits - NO Parity - 1 Stop Bit for Transmission - DLAB = 1
+	U0DLL = 97; // Set BAUD Rate as 9600 bps at 15 MHz VPB Clock (PCLK)
+	U0LCR = 0x03; // Disable Access to Divisor Latches
+	
+	// Infinite Loop (An embedded program does not stop):
+	while(1)
+	{
+		while(!(U0LSR & 0x01)); // Wait until UART0 is ready with the received data
+	  	data = U0RBR; // Read the received data from U0RBR
+		while(!(U0LSR & 0x20)); // Wait until UART0 is ready to send character    
+		U0THR = data; // Sent a character to UART Transmit Shift Register
+	}                               
+}
